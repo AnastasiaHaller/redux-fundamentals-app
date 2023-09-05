68b942f234de7ecf4d471e31376ee537d4eca18c
Anastasia Haller <anadana87@gmail.com>
2023-09-05 14:42:09 +0200

    implement functionality added increment and decrement actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,22 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
 
+  const dispatchFunction = useDispatch();
   const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatchFunction({
+      type: "increment",
+    });
+  };
+
+  const decrementHandler = () => {
+    dispatchFunction({
+      type: "decrement",
+    });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -11,6 +24,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={decrementHandler}>-</button>
+      </div>
       <button onClick={toggleCounterHandler}>Hide / Show</button>
     </main>
   );
