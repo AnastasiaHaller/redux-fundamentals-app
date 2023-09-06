d7b2a59be27f6cce7fd03d3da2f5720bc8abdf4f
Anastasia Haller <anadana87@gmail.com>
2023-09-06 09:23:45 +0200

    implement second state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ const Counter = () => {
 
   const dispatchFunction = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const isCounterInvisible = useSelector((state) => state.isCounterInvisible)
 
   const incrementHandler = () => {
     dispatchFunction({
@@ -18,14 +19,24 @@ const Counter = () => {
     });
   };
 
-  const toggleCounterHandler = () => {};
+  const increaseHandler = () => {
+    dispatchFunction({
+      type: "increase",
+      number: 10
+    });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatchFunction({type: "visibility"})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {!isCounterInvisible && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>+</button>
+        <button onClick={increaseHandler}>+10</button>
         <button onClick={decrementHandler}>-</button>
       </div>
       <button onClick={toggleCounterHandler}>Hide / Show</button>
